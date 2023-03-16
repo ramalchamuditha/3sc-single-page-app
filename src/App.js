3023ae88d281c5e2cf92c0bd913004dff7949166
Chamuditha <chamudithajayaweera@gmail.com>
2023-03-17 05:01:00 +0530

    Hosted on firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
 import Card from './components/Card';
 import './components/style.css'
